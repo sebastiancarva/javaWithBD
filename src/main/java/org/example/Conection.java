e97afdf4ec9257100b18e31bbc412430762159f2
Sebas Carvajal <scarvajal437@gmail.com>
2023-06-28 11:12:21 -0500

    se agrega modelo y request name y añadido de persona a bd dinamico

--- src/main/java/org/example/Conection.java
+++ src/main/java/org/example/Conection.java
@@ -1,16 +1,30 @@
 package org.example;
 
+import Models.Persona;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Scanner;
 
 public class Conection {
 
     public static void main(String[] args) {
-        conectionBD();
+
+        String name = requestName();
+        conectionBD(name);
+
     }
 
-    public static void conectionBD(){
+    public static String requestName(){
+        System.out.println("Introduce tu Nombre ");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.next();
+        return name;
+    }
+
+    public static void conectionBD(String name){
 
         //variables
         String jdbcDriver = "org.postgresql.Driver";
@@ -21,9 +35,13 @@ public class Conection {
         try (Connection connection = DriverManager.getConnection(BD,user,pass)) {
 
             Class.forName(jdbcDriver);
-            PreparedStatement statement = connection.prepareStatement("INSERT INTO persona (nombre) values ('Hoyos')");
+            PreparedStatement statement = connection.prepareStatement("INSERT INTO persona (nombre) values (?)");
+            statement.setString(1,name);
             statement.executeUpdate();
             statement.close();
+            System.out.println("Se agrego exitosamente a: " + name);
+
+
 
         } catch (Exception e) {
 
