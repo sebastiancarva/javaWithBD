3a155199a9453390bc89d63be781b7c272e10921
Sebas Carvajal <scarvajal437@gmail.com>
2023-06-28 13:28:54 -0500

    se agrega configuraciones

--- src/main/java/org/example/Conection.java
+++ src/main/java/org/example/Conection.java
@@ -6,14 +6,20 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Conection {
 
     public static void main(String[] args) {
 
-        String name = requestName();
-        conectionBD(name);
+        //String name = requestName();
+        //conectionBD(name);
+        List<Persona> personas = recoverList();
+        for (Persona persona : personas) {
+            System.out.println("id: " + persona.getId() + " Nombre: " + persona.getNombre());
+        }
 
     }
 
@@ -41,14 +47,42 @@ public class Conection {
             statement.close();
             System.out.println("Se agrego exitosamente a: " + name);
 
-
-
         } catch (Exception e) {
 
             System.out.println("Se produjo un error al intentar conectar con la BD" + e.getMessage());
 
         }
 
+    }
+
+    public static List<Persona> recoverList(){
+        //variables
+        List<Persona> personas = new ArrayList<>();
+        String jdbcDriver = "org.postgresql.Driver";
+        String BD = "jdbc:postgresql://localhost:5433/ejemplo2";
+        String user = "postgres";
+        String pass = "9908";
+
+        try (Connection connection = DriverManager.getConnection(BD,user,pass)) {
+
+            Class.forName(jdbcDriver);
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM persona");
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()){
+                Persona persona = new Persona();
+                persona.setId(resultSet.getInt("id"));
+                persona.setNombre(resultSet.getString("nombre"));
+                personas.add(persona);
+            }
+            statement.close();
+            resultSet.close();
+
+        } catch (Exception e) {
+
+            System.out.println("Se produjo un error al intentar traer los datos de la BD: " + e.getMessage());
+
+        }
+        return personas;
     }
 
 }
